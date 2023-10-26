9e9f414a55c9b06bca964094c50571a6919de833
proloypaul <proloyp23@gmail.com>
2023-10-27 01:09:17 +0600

    create blog posted model also regiater post blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PostBlog
 
 # Register your models here.
+@admin.register(PostBlog)
+class PostBlogAdmin(admin.ModelAdmin):
+    list_display = ["id", "creatorName", "title", "description", "category", "publishDate", "author"]
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Blogcreator(models.Model):
+class PostBlog(models.Model):
     creatorName = models.CharField(max_length=50)
     # blogImg = models.ImageField()
-    blogTitle = models.TimeField()
+    title = models.TextField()
     description = models.TextField()
-    blogCategory = models.CharField(max_length=50)
-    publishDate = models.DateTimeField()
-
+    category = models.CharField(max_length=50)
+    publishDate = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
