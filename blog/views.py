596944221e62f2b39ab2ac11203d6e96c25c11c9
proloypaul <proloyp23@gmail.com>
2023-10-27 01:20:05 +0600

    load posted blog data according to postblog model

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import PostBlog
 
-blogPost = [
-    {
-        "title": "what is OOP",
-        "description": "some description about our blog",
-        "author": "Suprit",
-        "pubilsh": "12/2/23"
-    },
-    {
-        "title": "How react dom work",
-        "description": "some description about our blog",
-        "author": "Suprit",
-        "pubilsh": "12/2/23"
-    }
-]
+
+# blogPost = [
+#     {
+#         "title": "what is OOP",
+#         "description": "some description about our blog",
+#         "author": "Suprit",
+#         "pubilsh": "12/2/23"
+#     },
+#     {
+#         "title": "How react dom work",
+#         "description": "some description about our blog",
+#         "author": "Suprit",
+#         "pubilsh": "12/2/23"
+#     }
+# ]
+
+
+
 
 def home(request):
-    return render(request, 'blog/home.html', {"blogPost": blogPost})
+    postedBlog = PostBlog.objects.all()
+    return render(request, 'blog/home.html', {"blogPost": postedBlog})
 
 def about(request):
     return render(request, 'blog/about.html', {"title":"about page"})
